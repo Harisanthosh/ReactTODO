caae6c11a3882459ed89b39483852d4294f3ee28
Harisan07 <harisanthosh94@gmail.com>
2018-09-06 13:31:17 +0530

    Toggle logic and add logic dispatcher added

--- src/components/ToDoDisplayList.js
+++ src/components/ToDoDisplayList.js
@@ -1,12 +1,32 @@
 import React from "react";
 import ListItem from "./ListItem";
 import { connect } from "react-redux";
+import { toggleItem } from "../actions/toggletodosActions";
 
 class ToDoDisplayList extends React.Component {
   constructor(props) {
     super(props);
+    this.toggleList = this.toggleList.bind(this);
+  }
+  toggleList() {
+    console.log("With props", this.props);
+    this.props.dispatch(toggleItem(this.props.id));
   }
   render() {
+    //don't make presentational components intelligent
+    let itemsJSX = null;
+    itemsJSX = this.props.listItems.map(element => {
+      // return
+      //const complete = element.complete == true? "complete" : "";
+      return (
+        <ListItem
+          todo={element.text}
+          complete={element.complete}
+          id={element.id}
+          onClick={this.toggleList}
+        />
+      );
+    });
     return (
       <div>
         <ul>
@@ -20,6 +40,29 @@ class ToDoDisplayList extends React.Component {
 }
 function mapStateToProps(state) {
   //tbd
+  if (state.menu == "ALL") {
+    return {
+      listItems: [state.todos]
+    };
+  } else if (state.menu == "ACTIVE") {
+    let activeItems = state.todos.map(element => {
+      if (element.complete == false) {
+        return element;
+      }
+    });
+    return {
+      listItems: activeItems
+    };
+  } else {
+    let completedItems = state.todos.map(element => {
+      if (element.complete == true) {
+        return element;
+      }
+    });
+    return {
+      listItems: completedItems
+    };
+  }
 }
 
 const displayListContainer = connect(mapStateToProps)(ToDoDisplayList);
--- src/components/ToDoSearch.js
+++ src/components/ToDoSearch.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
+import { addListItem } from "../actions/toggletodosActions";
 
 class ToDoSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem() {
+    console.log("Propse obtained", this.props);
+    const val = document.getElementById("todoText").value;
+    this.props.dispatch(addListItem(val));
+  }
   render() {
     return (
       <div>
